0b7a19f8101ca35cb25dc81fa1cd4fef982b7119
Daniil <harlok_312@mail.ru>
2020-05-26 20:04:56 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var numeber = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0)
+
+
+let persoe = {
+    name: "John",
+    age: "25",
+    isMarried: false
+};
+
+console.log(persoe['name']);
+
+let arr = ['plum.png', 'orang.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("hello world");
+
+//  let ansner = confilm("are you here?")
+
+//  console.log(ansner);
+
+let ansner = prompt("Есть вам 18?", "да")
+
+console.log(typeof(arr));
